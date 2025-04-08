14befe1c5736a6b60824467e5773a71f532cedba
HasandiLavanya <118769260+HasandiLavanya@users.noreply.github.com>
2025-04-09 04:19:08 +0530

    add a route to fletch country data

--- BackEnd/src/routes/countryRoutes.js
+++ BackEnd/src/routes/countryRoutes.js
@@ -5,3 +5,14 @@ const { validateApiKey } = require("../middleware/apiKeyMiddleware");
 const { logApiUsage } = require("../Utils/logger");
 
 const router = express.Router();
+
+router.get("/:name", authMiddleware, validateApiKey, async (req, res) => {
+    try {
+        await logApiUsage(req.user.id, req.originalUrl);
+
+        await getCountryInfo(req, res);
+    } catch (error) {
+        console.error("Error during country fetch:", error.message);
+        res.status(500).json({ error: "Failed to fetch country data or log usage" });
+    }
+});

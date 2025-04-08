6e07509f8dc24472f5b872457b4e77cc9ef4e7d0
HasandiLavanya <118769260+HasandiLavanya@users.noreply.github.com>
2025-04-09 02:58:36 +0530

    write fletch api key realted to user function write

--- BackEnd/src/controllers/adminController.js
+++ BackEnd/src/controllers/adminController.js
@@ -4,3 +4,17 @@ const {
     getUnusedApiKeys,
     getApiKeyOwners
 } = require("../models/apiKeyModel");
+
+const fetchUnusedApiKeys = (req, res) => {
+    const twoDaysAgo = new Date();
+    twoDaysAgo.setDate(twoDaysAgo.getDate() - 2);
+    const formattedDate = twoDaysAgo.toISOString().slice(0, 19).replace("T", " ");
+
+    getUnusedApiKeys(formattedDate, (err, rows) => {
+        if (err) {
+            console.error("Database error:", err);
+            return res.status(500).json({ error: "Database error" });
+        }
+        res.json(rows);
+    });
+};

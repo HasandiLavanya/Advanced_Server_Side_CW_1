f2bbbe99b9091ec3087eb974afbf2f633af8f61c
HasandiLavanya <118769260+HasandiLavanya@users.noreply.github.com>
2025-04-09 03:20:51 +0530

    update api key middleware

--- BackEnd/src/middleware/apiKeyMiddleware.js
+++ BackEnd/src/middleware/apiKeyMiddleware.js
@@ -1,2 +1,9 @@
 const db = require("../config/database");
 const { getUserByApiKey } = require("../models/apiKeyModel");
+
+const validateApiKey = (req, res, next) => {
+    const apiKey = req.headers["x-api-key"];
+
+    if (!apiKey) {
+        return res.status(403).json({ error: "API key required in 'x-api-key' header" });
+    }

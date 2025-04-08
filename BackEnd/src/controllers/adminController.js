c500e983d2d6d5e18e8893b5681adef9c297e595
HasandiLavanya <118769260+HasandiLavanya@users.noreply.github.com>
2025-04-09 03:40:38 +0530

    add fetchApiKeyOwners function to admin mid

--- BackEnd/src/controllers/adminController.js
+++ BackEnd/src/controllers/adminController.js
@@ -52,3 +52,12 @@ const fetchUnusedApiKeys = (req, res) => {
     const twoDaysAgo = new Date();
     twoDaysAgo.setDate(twoDaysAgo.getDate() - 2);
     const formattedDate = twoDaysAgo.toISOString().slice(0, 19).replace("T", " ");
+
+    getUnusedApiKeys(formattedDate, (err, rows) => {
+        if (err) {
+            console.error("Database error:", err);
+            return res.status(500).json({ error: "Database error" });
+        }
+        res.json(rows);
+    });
+};

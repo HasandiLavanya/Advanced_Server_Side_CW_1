1dbbcb3f2e1a9540590d1b65d82de4b7a1b48848
HasandiLavanya <118769260+HasandiLavanya@users.noreply.github.com>
2025-04-09 21:43:50 +0530

    updated admindash.js

--- Frontend/src/components/Dashboard/AdminDash/AdminDash.js
+++ Frontend/src/components/Dashboard/AdminDash/AdminDash.js
@@ -4,3 +4,15 @@ import { useNavigate } from "react-router-dom";
 import "../../Dashboard/AdminDash/AdminDash.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+const AdminDashboard = () => {
+    const [users, setUsers] = useState([]);
+    const [unusedApiKeys, setUnusedApiKeys] = useState([]);
+    const [apiKeyOwners, setApiKeyOwners] = useState([]);
+    const navigate = useNavigate();
+    const adminUsername = localStorage.getItem("username") || "Admin";
+  
+    useEffect(() => {
+      fetchUsers();
+      fetchUnusedApiKeys();
+    }, []);

74f166721b1ecacf8fd9248dfe470b4f75a48044
HasandiLavanya <118769260+HasandiLavanya@users.noreply.github.com>
2025-04-09 14:52:06 +0530

    updated server.js

---
+++ BackEnd/src/server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const db = require("./src/config/database"); 
+const cookieParser = require("cookie-parser");

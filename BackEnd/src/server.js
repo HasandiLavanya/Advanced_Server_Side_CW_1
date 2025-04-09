4153fdb641bd6557f06b031a9439e9adefeae730
HasandiLavanya <118769260+HasandiLavanya@users.noreply.github.com>
2025-04-09 14:52:25 +0530

    updated server.js

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -3,3 +3,12 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const db = require("./src/config/database"); 
 const cookieParser = require("cookie-parser");
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:3001", 
+    credentials: true               
+  }));
+app.use(cookieParser());

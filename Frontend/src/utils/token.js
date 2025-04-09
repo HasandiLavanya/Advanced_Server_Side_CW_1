529ebb24c9c61555fc853f34844d3c5037fd5ed2
HasandiLavanya <118769260+HasandiLavanya@users.noreply.github.com>
2025-04-09 21:48:41 +0530

    token .js added

---
+++ Frontend/src/utils/token.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const refreshToken = async () => {
+  try {
+    await axios.post("http://localhost:3000/auth/refresh-token", {}, { withCredentials: true });
+    const newExp = Date.now() / 1000 + 3600; // 1 hour
+    localStorage.setItem("exp", newExp.toString());
+  } catch (err) {
+    console.error("Refresh token failed", err);
+    localStorage.clear();
+    window.location.href = "/login";
+  }
+};
